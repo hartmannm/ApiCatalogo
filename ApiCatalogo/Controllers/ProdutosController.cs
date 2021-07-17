4fcaaa8e6c4becd2eb85209a3497f42ce631c7e9
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-17 15:36:03 -0300

    Criada implementação básica do controller de produtos
    - Criado controller com métodos básicos
    - Por hora, lógica de alteração de estado fica no controller

---
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,69 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiCatalogo.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produtos
+                .AsNoTracking()
+                .ToList();
+        }
+
+        [HttpGet("{id}", Name = "ObterProduto")]
+        public ActionResult<Produto> GetById(int id)
+        {
+            var produto = _context.Produtos
+                .AsNoTracking()
+                .FirstOrDefault(p => p.ProdutoId == id);
+            if (produto == null)
+                return NotFound();
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Incluir([FromBody] Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Alterar(int id, [FromBody] Produto produto)
+        {
+            if (id != produto.ProdutoId)
+                return BadRequest();
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Produto> Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto == null)
+                return NotFound();
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return produto;
+        }
+    }
+}
