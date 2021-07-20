9e0ecef1d95c1237b59d62e490ecf47dd73fd7a7
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-20 13:55:05 -0300

    Implementadas validações personalizadas de atributos do produto

--- ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/Models/Produto.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using ApiCatalogo.Validations;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiCatalogo.Models
 {
     [Table("Produtos")]
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int ProdutoId { get; set; }
 
         [Required]
         [MaxLength(80)]
+        [PrimeiraLetraMaiuscula]
         public string Nome { get; set; }
 
         [Required]
@@ -28,5 +31,21 @@ namespace ApiCatalogo.Models
         public DateTime DataCadastro { get; set; }
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Descricao))
+            {
+                var primeiraLetra = this.Descricao[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("A primeira letra da descrição deve ser maiúscula", new[] { nameof(Descricao) });
+                }
+            }
+            if (Estoque <= 0)
+            {
+                yield return new ValidationResult("O estoque deve ser maior que zero", new[] { nameof(Estoque) });
+            }
+        }
     }
 }
