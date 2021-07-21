a8550fc1bfc6ff2ea4a39ec56d416441daca7814
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-21 18:35:02 -0300

    Criadas interfaces e implementações de repositórios
    - Criada interface de repositório genérico
    - Criada implementação do repositório genérico
    - Criadas interfaces e implementações de repositórios para categoria e produto

---
+++ ApiCatalogo/Repository/CategoriaRepository.cs
@@ -0,0 +1,19 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ApiCatalogo.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Categoria> GetCategoriasProdutos()
+        {
+            return Get().Include(c => c.Produtos);
+        }
+    }
+}
---
+++ ApiCatalogo/Repository/ProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiCatalogo.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Produto> GetProdutosPorPreco()
+        {
+            return Get().OrderBy(p => p.Preco).ToList();
+        }
+    }
+}
